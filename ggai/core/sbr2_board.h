409f11a8061ccd90f2aefad23ff2f2d85d6373c0
guyat <107256527+guyat@users.noreply.github.com>
2026-03-14 19:25:23 +0900

    Add files via upload

---
+++ ggai/core/sbr2_board.h
@@ -0,0 +1,105 @@
+#pragma once
+
+#include <array>
+#include <cstdint>
+
+using i8  = int8_t;
+using u8  = uint8_t;
+using i32 = int32_t;
+
+class SBR2Board
+{
+public:
+    static constexpr i8 WIDTH  = 13;
+    static constexpr i8 HEIGHT = 11;
+
+    enum class CellType : u8
+    {
+        EMPTY = 0,
+        HARD_BLOCK,
+        BOMB
+    };
+
+private:
+    std::array<std::array<CellType, WIDTH>, HEIGHT> cells_;
+
+public:
+    SBR2Board()
+    {
+        initialize();
+    }
+
+    void initialize()
+    {
+        for (i8 y = 0; y < HEIGHT; ++y) {
+            for (i8 x = 0; x < WIDTH; ++x) {
+                cells_[y][x] = CellType::EMPTY;
+            }
+        }
+
+        // ハードブロック: (x % 2 == 1 && y % 2 == 1)
+        for (i8 y = 0; y < HEIGHT; ++y) {
+            for (i8 x = 0; x < WIDTH; ++x) {
+                if ((x % 2 == 1) && (y % 2 == 1)) {
+                    cells_[y][x] = CellType::HARD_BLOCK;
+                }
+            }
+        }
+    }
+
+    bool is_inside(i8 x, i8 y) const
+    {
+        return (0 <= x && x < WIDTH && 0 <= y && y < HEIGHT);
+    }
+
+    CellType get_cell(i8 x, i8 y) const
+    {
+        return cells_[y][x];
+    }
+
+    void set_cell(i8 x, i8 y, CellType cell)
+    {
+        cells_[y][x] = cell;
+    }
+
+    bool is_hard_block(i8 x, i8 y) const
+    {
+        if (!is_inside(x, y)) {
+            return false;
+        }
+        return get_cell(x, y) == CellType::HARD_BLOCK;
+    }
+
+    bool is_bomb(i8 x, i8 y) const
+    {
+        if (!is_inside(x, y)) {
+            return false;
+        }
+        return cells_[y][x] == CellType::BOMB;
+    }    
+
+    // bool is_bomb(i8 x, i8 y) const
+    // {
+    //     if (!is_inside(x, y)) {
+    //         return false;
+    //     }
+    //     return get_cell(x, y) == CellType::BOMB;
+    // }
+
+    bool is_empty(i8 x, i8 y) const
+    {
+        if (!is_inside(x, y)) {
+            return false;
+        }
+        return get_cell(x, y) == CellType::EMPTY;
+    }
+
+    bool is_passable(i8 x, i8 y) const
+    {
+        if (!is_inside(x, y)) {
+            return false;
+        }
+
+        return get_cell(x, y) == CellType::EMPTY;
+    }
+};
